72e3aae8a563fdbdf34726acb0ddd479e7bfc8e5
Heitor Glockner <mai@heitorglockner.com>
2017-01-04 18:50:36 -0200

    indent using tabs

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -27,33 +27,33 @@ var Index = {
 		var self = this;
 
 		self.destaqueHome();
-		$("#phone").mask("(99) 9999-9999?9")
-	        .focusout(function (event) {  
-	            var target, phone, element;  
-	            target = (event.currentTarget) ? event.currentTarget : event.srcElement;  
-	            phone = target.value.replace(/\D/g, '');
-	            element = $(target);  
-	            element.unmask();  
-	            if(phone.length > 10) {  
-	                element.mask("(99) 99999-999?9");  
-	            } else {  
-	                element.mask("(99) 9999-9999?9");  
-	            }  
-	        });
+		$("#phone").mask("(99) 9999-9999?9");
+			.focusout(function (event) {
+				var target, phone, element;
+				target = (event.currentTarget) ? event.currentTarget : event.srcElement;
+				phone = target.value.replace(/\D/g, '');
+				element = $(target);
+				element.unmask();
+				if(phone.length > 10) {  
+					element.mask("(99) 99999-999?9");
+				} else {  
+					element.mask("(99) 9999-9999?9");
+				}
+			});
 
-	    $("#phone2").mask("(99) 9999-9999?9")
-	        .focusout(function (event) {  
-	            var target, phone, element;  
-	            target = (event.currentTarget) ? event.currentTarget : event.srcElement;  
-	            phone = target.value.replace(/\D/g, '');
-	            element = $(target);  
-	            element.unmask();  
-	            if(phone.length > 10) {  
-	                element.mask("(99) 99999-999?9");  
-	            } else {  
-	                element.mask("(99) 9999-9999?9");  
-	            }
-	        });
+		$("#phone2").mask("(99) 9999-9999?9")
+			.focusout(function (event) {
+				var target, phone, element;
+				target = (event.currentTarget) ? event.currentTarget : event.srcElement;
+				phone = target.value.replace(/\D/g, '');
+				element = $(target);
+				element.unmask();
+				if(phone.length > 10) {  
+					element.mask("(99) 99999-999?9");
+				} else {  
+					element.mask("(99) 9999-9999?9");
+				}
+			});
 
 	    $("input[name='cep']").mask("99999-999", {
 			completed:function () {
